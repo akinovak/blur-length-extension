ed06b7c29b289ee2ecefcf926c67416f916610e9
Andrija <akinovak@gmail.com>
2023-09-04 19:01:34 +0200

    hide on video page too

--- content.js
+++ content.js
@@ -13,22 +13,48 @@ window.onload = async () => {
     });
 
     isActive = await getExtensionStatus()
-    updateSpanOuterText()
+    hide()
 
     const observer = new MutationObserver((_) => {
-        updateSpanOuterText()
+        hide()
     });
 
     observer.observe(document, observerConfig);
 };
 
-
-const updateSpanOuterText = () => {
+const hide = () => {
     if (!isActive) return;
+    updateThumbnails()
+    removePlayerTime()
+    removeProgressBar()
+}
+
+const updateThumbnails = () => {
     const spans = document.querySelectorAll('span#text.style-scope.ytd-thumbnail-overlay-time-status-renderer');
     spans.forEach((span) => {
         span.outerText = "Hidden";
     });
+}
+
+const updateThumbnailsVideo = () => {
+    const spans = document.querySelectorAll('span#text.style-scope.ytd-thumbnail-overlay-time-status-renderer');
+    spans.forEach((span) => {
+        span.outerText = "Hidden";
+    });
+}
+
+const removePlayerTime = () => {
+    const element = document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > div.ytp-time-display.notranslate")
+    if (!element) return
+
+    element.style.display = "none"
+}
+
+const removeProgressBar = () => {
+    const element = document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-progress-bar-container > div.ytp-progress-bar")
+    if (!element) return
+
+    element.style.display = "none"
 }
 
 const getExtensionStatus = async() => {
